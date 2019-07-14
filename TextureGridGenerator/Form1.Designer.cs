ee45678e6a24a23c81fac07a46e4a91434ec239b
Zibbo <sebastian.wittmann97@web.de>
2019-07-14 15:01:16 +0200

    initial commit

---
+++ TextureGridGenerator/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace TextureGridGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resolutionLabel = new System.Windows.Forms.Label();
+            this.colorCheckBox = new System.Windows.Forms.CheckBox();
+            this.smallGridCheckbox = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(525, 542);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.smallGridCheckbox);
+            this.groupBox2.Controls.Add(this.colorCheckBox);
+            this.groupBox2.Controls.Add(this.resolutionLabel);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.trackBar1);
+            this.groupBox2.Location = new System.Drawing.Point(543, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(321, 152);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Settings";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(543, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(321, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(6, 35);
+            this.trackBar1.Maximum = 5;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(309, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Resolution:";
+            // 
+            // resolutionLabel
+            // 
+            this.resolutionLabel.AutoSize = true;
+            this.resolutionLabel.Location = new System.Drawing.Point(72, 19);
+            this.resolutionLabel.Name = "resolutionLabel";
+            this.resolutionLabel.Size = new System.Drawing.Size(48, 13);
+            this.resolutionLabel.TabIndex = 2;
+            this.resolutionLabel.Text = "512x512";
+            // 
+            // colorCheckBox
+            // 
+            this.colorCheckBox.AutoSize = true;
+            this.colorCheckBox.Location = new System.Drawing.Point(9, 86);
+            this.colorCheckBox.Name = "colorCheckBox";
+            this.colorCheckBox.Size = new System.Drawing.Size(55, 17);
+            this.colorCheckBox.TabIndex = 3;
+            this.colorCheckBox.Text = "Colors";
+            this.colorCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // smallGridCheckbox
+            // 
+            this.smallGridCheckbox.AutoSize = true;
+            this.smallGridCheckbox.Location = new System.Drawing.Point(9, 109);
+            this.smallGridCheckbox.Name = "smallGridCheckbox";
+            this.smallGridCheckbox.Size = new System.Drawing.Size(73, 17);
+            this.smallGridCheckbox.TabIndex = 4;
+            this.smallGridCheckbox.Text = "Small Grid";
+            this.smallGridCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(543, 509);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(321, 45);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(543, 458);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(321, 45);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Generate all variations...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 561);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(889, 600);
+            this.Name = "Form1";
+            this.Text = "Texture Grid Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label resolutionLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.CheckBox colorCheckBox;
+        private System.Windows.Forms.CheckBox smallGridCheckbox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TextureGridGenerator/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TextureGridGenerator
+{
+    public partial class Form1 : Form
+    {
+        public int resolution = 512;
+        public Bitmap grid = new Bitmap(256, 256);
+        Pen blackPen = new Pen(Color.Black, 1);
+        Pen greyPen = new Pen(Color.LightGray, 0.25f);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<PointF> getCorners(RectangleF r)
+        {
+            return new List<PointF>() { r.Location, new PointF(r.Right, r.Top),
+            new PointF(r.Right, r.Bottom), new PointF(r.Left, r.Bottom)};
+        }
+
+        public static Color medianColor(List<Color> cols)
+        {
+            int c = cols.Count;
+            return Color.FromArgb(cols.Sum(x => x.A) / c, cols.Sum(x => x.R) / c,
+                cols.Sum(x => x.G) / c, cols.Sum(x => x.B) / c);
+        }
+
+        public void generate(int resolution, bool color, bool smallGrid)
+        {
+            grid.Dispose();
+            grid = new Bitmap(resolution, resolution);
+            Graphics gfx = Graphics.FromImage(grid);
+            SolidBrush white = new SolidBrush(Color.White);
+            SolidBrush black = new SolidBrush(Color.Black);
+            if (color)
+            {
+                Rectangle r = new Rectangle(0, 0, resolution, resolution);
+                List<Color> colors = new List<Color> { Color.FromArgb(255, 255, 0, 0), Color.FromArgb(255, 0, 255, 0), Color.FromArgb(255, 0, 255, 255), Color.FromArgb(255, 255, 0, 255) };
+                for (int y = 0; y < r.Height; y++)
+                {
+
+                    using (PathGradientBrush pgb = new PathGradientBrush(getCorners(r).ToArray()))
+                    {
+                        pgb.CenterColor = medianColor(colors);
+                        pgb.SurroundColors = colors.ToArray();
+                        gfx.FillRectangle(pgb, 0, y, r.Width, 1);
+                    }
+                }
+            }
+            else
+            {
+                gfx.FillRectangle(white, 0, 0, resolution, resolution);
+            }
+
+            if (smallGrid)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        gfx.DrawLine(greyPen, ((resolution / 50) + (i * (resolution / 50))) + j * (resolution / 10), 0, ((resolution / 50) + (i * (resolution / 50))) + j * (resolution / 10), resolution);
+                        gfx.DrawLine(greyPen, 0, ((resolution / 50) + (i * (resolution / 50))) + j * (resolution / 10), resolution, ((resolution / 50) + (i * (resolution / 50))) + j * (resolution / 10));
+                    }
+                }
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                gfx.DrawLine(blackPen, (resolution / 10) + (i * (resolution / 10)), 0, (resolution / 10) + (i * (resolution / 10)), resolution);
+                gfx.DrawLine(blackPen, 0, (resolution / 10) + (i * (resolution / 10)), resolution, (resolution / 10) + (i * (resolution / 10)));
+            }
+
+            gfx.DrawString("0,0", new Font(FontFamily.GenericSansSerif, resolution / 30), black, resolution / 80, resolution / 50);
+            gfx.DrawString("1,0", new Font(FontFamily.GenericSansSerif, resolution / 30), black, resolution - (resolution / 11), resolution / 50);
+            gfx.DrawString("0,1", new Font(FontFamily.GenericSansSerif, resolution / 30), black, resolution / 80, resolution - (resolution / 12));
+            gfx.DrawString("1,1", new Font(FontFamily.GenericSansSerif, resolution / 30), black, resolution - (resolution / 11), resolution - (resolution / 12));
+
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    gfx.DrawString("0." + i, new Font(FontFamily.GenericSansSerif, resolution / 50), black, i * (resolution / 10), j * (resolution / 10));
+                }
+            }
+
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    gfx.DrawString("0." + i, new Font(FontFamily.GenericSansSerif, resolution / 50), black, j * (resolution / 10) + (resolution / 18), i * (resolution / 10) + (resolution / 15));
+                }
+            }
+            gfx.Dispose();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            generate(resolution, colorCheckBox.Checked, smallGridCheckbox.Checked);
+
+            pictureBox1.BackgroundImage = (Image)grid;
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            switch (trackBar1.Value)
+            {
+                case 1:
+                    resolutionLabel.Text = "512x512";
+                    resolution = 512;
+                    blackPen.Width = 1;
+                    greyPen.Width = 0.25f;
+                    break;
+                case 2:
+                    resolutionLabel.Text = "1024x1024";
+                    resolution = 1024;
+                    blackPen.Width = 2;
+                    greyPen.Width = 0.5f;
+                    break;
+                case 3:
+                    resolutionLabel.Text = "2048x2048";
+                    resolution = 2048;
+                    blackPen.Width = 4;
+                    greyPen.Width = 1;
+                    break;
+                case 4:
+                    resolutionLabel.Text = "4096x4096";
+                    resolution = 4096;
+                    blackPen.Width = 8;
+                    greyPen.Width = 2;
+                    break;
+                case 5:
+                    resolutionLabel.Text = "8192x8192";
+                    resolution = 8192;
+                    blackPen.Width = 16;
+                    greyPen.Width = 4;
+                    break;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "PNG image|*.png|JPG image|*.jpg";
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                if (sfd.FileName.EndsWith(".png"))
+                {
+                    grid.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                }  else if (sfd.FileName.EndsWith(".jpg"))
+                {
+                    grid.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();  
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                string basePath = fbd.SelectedPath;
+
+                float bl = blackPen.Width;
+                float gr = greyPen.Width;
+
+                blackPen.Width = 1;
+                greyPen.Width = 0.25f;
+                generate(512, false, false);
+                grid.Save(basePath + "\\bw_512.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(512, true, false);
+                grid.Save(basePath + "\\color_512.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(512, false, true);
+                grid.Save(basePath + "\\bw_grid_512.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(512, true, true);
+                grid.Save(basePath + "\\color_grid_512.png", System.Drawing.Imaging.ImageFormat.Png);
+                System.GC.Collect();
+
+                blackPen.Width = 2;
+                greyPen.Width = 0.5f;
+                generate(1024, false, false);
+                grid.Save(basePath + "\\bw_1024.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(1024, true, false);
+                grid.Save(basePath + "\\color_1024.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(1024, false, true);
+                grid.Save(basePath + "\\bw_grid_1024.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(1024, true, true);
+                grid.Save(basePath + "\\color_grid_1024.png", System.Drawing.Imaging.ImageFormat.Png);
+                System.GC.Collect();
+
+                blackPen.Width = 4;
+                greyPen.Width = 1;
+                generate(2048, false, false);
+                grid.Save(basePath + "\\bw_2048.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(2048, true, false);
+                grid.Save(basePath + "\\color_2048.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(2048, false, true);
+                grid.Save(basePath + "\\bw_grid_2048.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(2048, true, true);
+                grid.Save(basePath + "\\color_grid_2048.png", System.Drawing.Imaging.ImageFormat.Png);
+                System.GC.Collect();
+
+                blackPen.Width = 8;
+                greyPen.Width = 2;
+                generate(4096, false, false);
+                grid.Save(basePath + "\\bw_4096.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(4096, true, false);
+                grid.Save(basePath + "\\color_4096.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(4096, false, true);
+                grid.Save(basePath + "\\bw_grid_4096.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(4096, true, true);
+                grid.Save(basePath + "\\color_grid_4096.png", System.Drawing.Imaging.ImageFormat.Png);
+                System.GC.Collect();
+
+
+                blackPen.Width = 16;
+                greyPen.Width = 4;
+                generate(8192, false, false);
+                grid.Save(basePath + "\\bw_8192.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(8192, true, false);
+                grid.Save(basePath + "\\color_8192.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(8192, false, true);
+                grid.Save(basePath + "\\bw_grid_8192.png", System.Drawing.Imaging.ImageFormat.Png);
+                generate(8192, true, true);
+                grid.Save(basePath + "\\color_grid_8192.png", System.Drawing.Imaging.ImageFormat.Png);
+                System.GC.Collect();
+
+                blackPen.Width = bl;
+                greyPen.Width = gr;
+            }
+        }
+    }
+}
